13a4c759ad77e874a705858c4bf591d15726aa10
DcWind <danielekwuazi@gmail.com>
2017-01-21 00:01:38 +0700

    onColor func

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+  color:string;
+
+  onColor(color){
+    console.log("color:",color);
+  }
 }
