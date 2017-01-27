99cc790f892ffdbbf96a2f4cbbd5821e93856d6e
DcWind <danielekwuazi@gmail.com>
2017-01-27 23:56:53 +0700

    color-previewer component

---
+++ src/app/components/color-previewer/color-previewer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-color-previewer',
+  templateUrl: './color-previewer.component.html',
+  styleUrls: ['./color-previewer.component.css']
+})
+export class ColorPreviewerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
